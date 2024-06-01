b1a538749f9b3283475ad11851c89de91ac7e0ff
김기범 <ikkb77@inection.co.kr>
2024-06-01 21:05:50 +0900

    poi 라이브러리와 poi-ooxml 라이브러리 취약점 패치 (3.17에서 5.24로 마이그레이션)

--- test.bat
+++ test.bat
@@ -1,6 +1,7 @@
 rem java -jar ./target/XMLtoExcelMaven-0.0.1-SNAPSHOT-jar-with-dependencies.jar FortifyLoginProject_Security_Report.xml FortifyLoginProject_Security_Report.xlsx
 rem java -jar ./target/XMLtoExcelMaven-0.0.1-SNAPSHOT-jar-with-dependencies.jar test.xml test.xlsx
 rem java -jar ./target/XMLtoExcelMaven-0.0.1-SNAPSHOT-jar-with-dependencies.jar kisa49.xml kisa49.xlsx
-java -jar ./target/XMLtoExcelMaven-0.0.1-SNAPSHOT-jar-with-dependencies.jar FortifyLoginProject_-_kisa491.xml FortifyLoginProject_-_kisa491.xsx
+rem java -jar ./target/XMLtoExcelMaven-0.0.1-SNAPSHOT-jar-with-dependencies.jar FortifyLoginProject_-_kisa491.xml FortifyLoginProject_-_kisa491.xlsx
+java -jar ./target/XMLtoExcelMaven-0.0.1-SNAPSHOT-jar-with-dependencies.jar FortifyWebGoat_-_kisa49.xml FortifyWebGoat_-_kisa49.xlsx
 
 pause
